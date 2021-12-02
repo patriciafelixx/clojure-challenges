6fc973c71ecab41057905681c9ca486bdff5f0b2
patriciafelixx <patriciafelixx@outlook.com>
2021-12-02 12:13:09 -0300

    Starting the second challenge

---
+++ challenge-semana02/README.md
@@ -0,0 +1,12 @@
+## Challenge Backend - Semana 2
+
+**Escrever testes automatizados em Clojure do código que simula o cartão de crédito de um(a) cliente, desenvolvido na semana 01**.
+
+O que esse código precisa fazer?
+
+* Teste da função que adiciona uma compra na lista de compras realizadas;
+* Teste da função que lista as compras realizadas;
+* Teste da função que realiza o cálculo dos gastos agrupados por categoria.
+
+
+Bom projeto!
---
+++ challenge-semana02/src/creditcard/functions.clj
@@ -0,0 +1,70 @@
+(ns creditcard.functions
+  (:require [creditcard.db :as c.db]
+            [java-time :as jt]))
+
+; ------------------------------------------------------------------------------------------------------
+(println "=> TOTAL BY CATEGORIES")
+
+(defn total-value [records]
+  (->> records
+       (map :value)
+       (reduce +)))
+
+(defn by-item
+  [[category records]]
+  {:category     category
+   :num-records  (count records)
+   :total-amount (total-value records)})
+
+(->> (c.db/all-records)
+     (group-by :category)
+     (map by-item)
+     println)
+
+; ------------------------------------------------------------------------------------------------------
+(println "=> TOTAL BY PLACES")
+
+(defn by-place
+  [[place records]]
+  {:place        place
+   :num-records  (count records)
+   :total-amount (total-value records)})
+
+(->> (c.db/all-records)
+     (group-by :place)
+     (map by-place)
+     println)
+
+; ------------------------------------------------------------------------------------------------------
+(println "=> CREDIT CARD INVOICE")
+
+(defn invoice
+  [card-number period]
+  (->> (c.db/all-records)
+       (filter #(= card-number (:creditcard-number %)))
+       (filter #(= (jt/year (jt/local-date "dd/MM/yyyy" period)) (jt/year (jt/local-date-time (:date %)))))
+       (filter #(= (jt/month (jt/local-date "dd/MM/yyyy" period)) (jt/month (jt/local-date-time (:date %)))))
+       total-value
+       println))
+
+(invoice "1654658431352967" "01/11/2021")
+
+; ------------------------------------------------------------------------------------------------------
+(defn filter-by-place
+  [place]
+  (println "=> RECORDS MADE IN" place)
+  (->> (c.db/all-records)
+       (filter #(= place (:place %)))
+       println))
+
+(filter-by-place "Padaria")
+
+; ------------------------------------------------------------------------------------------------------
+(defn filter-by-value
+  [value]
+  (println "=> RECORDS WORTH" value)
+  (->> (c.db/all-records)
+       (filter #(= value (:value %)))
+       println))
+
+(filter-by-value 100.00)
